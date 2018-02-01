f6e96bb6469ef656d2e91a5cb5ea9e3d66ebb5de
trinanumana <31747072+trinanumana@users.noreply.github.com>
2018-02-01 13:21:44 -0800

    Add files via upload

---
+++ strangerthings.rb
@@ -0,0 +1,44 @@
+use_bpm 160
+
+live_loop :main do
+  with_synth :blade do
+    play :c4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+    play :g4
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :c5
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :g4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+  end
+end
+
+live_loop :background do
+  use_bpm 80
+  with_synth :saw do
+    play :d, amp: 0.2
+    sleep 0.5
+    play :e, amp: 0.2
+    sleep 0.5
+    play :g, amp: 0.2
+    sleep 0.5
+    play :b, amp: 0.2
+    sleep 0.5
+    play :c, amp: 0.2
+    sleep 0.5
+    play :b, amp: 0.2
+    sleep 0.5
+    play :g, amp: 0.2
+    sleep 0.5
+    play :e, amp: 0.2
+    sleep 0.5
+  end
+end
